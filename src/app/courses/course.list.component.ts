f0b08d5e790a64b45b5ebb74eb9c61323a791e77
unknown <edsonfire@gmail.com>
2021-09-12 23:45:58 -0300

    segunda parte

--- src/app/courses/course.list.component.ts
+++ src/app/courses/course.list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { Course } from "./course";
+import { Course } from "./Course";
+import { CourseService } from "./course.service";
 
 
 @Component({
@@ -8,41 +9,13 @@ import { Course } from "./course";
 })
 export class CourseListComponent implements OnInit{
 
+    constructor (private courseService: CourseService){
+        this.curses = this.courseService.retrieveAll();
+    }
 curses : Course[] = [];
 
 ngOnInit(): void {
-   this.curses = [
-       {
-        id: 1,
-        name: "Angular Básico",
-        imageURL: "./assets/images/forms.png",
-        price: 100,
-        code: 'c1',
-        duration: 6,
-        rating: 5,
-        releaseDate:'10/01/2020'
-       },
-       {
-        id: 2,
-        name: "Angular Intermediário",
-        imageURL: "./assets/images/http.png",
-        price: 250,
-        code: 'c2',
-        duration: 15,
-        rating: 4,
-        releaseDate:'22/04/2021'
-       },
-       {
-        id: 3,
-        name: "Angular Avançado",
-        imageURL: "./assets/images/router.png",
-        price: 350,
-        code: 'c3',
-        duration: 22,
-        rating: 5, 
-        releaseDate:'05/06/2021'
-       }
-   ]
+   
 }
 
 
