eb18f49fffe1e64f67a290d30602a3d6789794ef
unknown <edsonfire@gmail.com>
2021-09-14 00:34:27 -0300

    mais classes

--- src/app/courses/course.list.component.ts
+++ src/app/courses/course.list.component.ts
@@ -4,20 +4,33 @@ import { CourseService } from "./course.service";
 
 
 @Component({
-    selector:'app-course-list', 
+    
     templateUrl:'./course.list.component.html'
 })
 export class CourseListComponent implements OnInit{
 
+    _filterBy!:string;
+    _curses : Course[] = [];
+    filteredCurses : Course[] = [];
+
     constructor (private courseService: CourseService){
-        this.curses = this.courseService.retrieveAll();
+        this._curses = this.courseService.retrieveAll();
+        this.filteredCurses = this._curses;
+
     }
-curses : Course[] = [];
+
 
 ngOnInit(): void {
    
 }
 
+set filter(value:string){
+    this._filterBy=value;
+    this.filteredCurses = this._curses.filter((course:Course)=> course.name.toLowerCase().indexOf(this._filterBy.toLowerCase())> -1);
+}
 
+get filter(){
+   return  this._filterBy;
+}
 
 }
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-navbar', 
+    templateUrl:'./nav-bar.component.html'
+})
+export class NavBarComponent
+{
+
+
+
+}
